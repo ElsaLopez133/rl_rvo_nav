3bb2369073f3d79bff2b5ddcf6c00ea744a9f87a
Lopez Perez Elsa <elsa.lopez_perez@nokia.com>
2023-06-23 17:32:46 +0200

    Experiments

--- rl_rvo_nav/plot_success_rate.py
+++ rl_rvo_nav/plot_success_rate.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 import matplotlib.gridspec as gridspec
+import matplotlib.pylab as pylab
+
+params = {'legend.fontsize': 'x-large',
+         'axes.labelsize': 'x-large',
+         'axes.titlesize':'x-large',
+         'xtick.labelsize':'x-large',
+         'ytick.labelsize':'x-large'}
+pylab.rcParams.update(params)
 
 
 df = pd.DataFrame(columns = ['radius', 'sr'])
@@ -24,4 +32,46 @@ title = 'Success rate for different radius values'
 axes.grid()
 axes.set_title(title)
 plt.show()
-plt.savefig(name)
+plt.savefig(name, bbox_inches='tight')
+
+
+df = pd.DataFrame(columns = ['number of robots', 'sr'])
+radius = [5,10,12,15,18,20]
+sr = [0,0,0,0,0,0]
+df['number_robots'] = radius
+df['sr'] = sr
+
+fig = plt.figure(figsize = [5,5])
+axes = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # left, bottom, width, height (range 0 to 1)
+axes.set_ylabel('success rate (%)')
+axes.set_xlabel('number of robots')
+
+axes.plot(np.array(df['number_robots']),np.array(df['sr']), color = 'red', marker = '.')
+
+name = '/home/rosfr/catkin_ws/src/kale_bot/external/rl_rvo_nav/rl_rvo_nav/success_rate_number_robots.png'
+title = 'Success rate for different number of robots'
+axes.grid()
+axes.set_title(title)
+plt.show()
+plt.savefig(name, bbox_inches='tight')
+
+
+df = pd.DataFrame(columns = ['linear and angular speed (m/s, rad/s)', 'sr'])
+radius = [0.5,0.55,0.60,0.65,0.75,1.0,1.5]
+sr = [0,0,0,0,100,100,100]
+df['speed'] = radius
+df['sr'] = sr
+
+fig = plt.figure(figsize = [5,5])
+axes = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # left, bottom, width, height (range 0 to 1)
+axes.set_ylabel('success rate (%)')
+axes.set_xlabel('linear and angular speed (m/s, rad/s)')
+
+axes.plot(np.array(df['speed']),np.array(df['sr']), color = 'red', marker = '.')
+
+name = '/home/rosfr/catkin_ws/src/kale_bot/external/rl_rvo_nav/rl_rvo_nav/success_rate_speed.png'
+title = 'Success rate for different speed values'
+axes.grid()
+axes.set_title(title)
+plt.show()
+plt.savefig(name, bbox_inches='tight')
--- rl_rvo_nav/plot_vary_num_robots.py
+++ rl_rvo_nav/plot_vary_num_robots.py
@@ -20,8 +20,30 @@ axes.set_xlabel('number of robots (m)')
 axes.plot(np.array(df['number_robots']),np.array(df['sr']), color = 'red', marker = '.')
 
 name = '/home/rosfr/catkin_ws/src/kale_bot/external/rl_rvo_nav/rl_rvo_nav/success_rate_number_robots.png'
-title = 'Success rate for different number of robots and speed 0.5m/s - 0.5rad/s'
+title = 'Success rate for different number of robots'
 axes.grid()
 axes.set_title(title)
 plt.show()
-plt.savefig(name)
+plt.savefig(name, bbox_inches='tight')
+
+
+df = pd.DataFrame(columns = ['linear and angular speed (m/s, rad/s)', 'sr'])
+radius = [0.5,0.55,0.60,0.65,0.75,1.0,1.5]
+sr = [0,0,0,0,100,100,100]
+df['speed'] = radius
+df['sr'] = sr
+
+fig = plt.figure(figsize = [5,5])
+axes = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # left, bottom, width, height (range 0 to 1)
+axes.set_ylabel('success rate (%)')
+axes.set_xlabel('linear and angular speed (m/s, rad/s)')
+
+axes.plot(np.array(df['speed']),np.array(df['sr']), color = 'red', marker = '.')
+
+name = '/home/rosfr/catkin_ws/src/kale_bot/external/rl_rvo_nav/rl_rvo_nav/success_rate_speed.png'
+title = 'Success rate for different speed values'
+axes.grid()
+axes.set_title(title)
+plt.show()
+plt.savefig(name, bbox_inches='tight')
+
